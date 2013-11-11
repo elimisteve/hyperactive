2c2b291803b7b51be93cecfb477cf763d4616e49
Steve Phillips <elimisteve@gmail.com>
2013-11-10 23:30:49 -0800

    Added first alpha version: v0.0.1

---
+++ services.go
@@ -0,0 +1,125 @@
+// Steve Phillips / elimisteve
+// 2013.11.10
+
+package main
+
+import (
+	"./types"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"runtime"
+	"time"
+)
+
+var (
+	router = mux.NewRouter()
+)
+
+const (
+	DATABASE_NAME = "hyperactive"
+	FAKE_URL      = "http://activity.hype"
+	SECRET_KEY    = ""
+)
+
+func init() {
+	if DATABASE_NAME == "" {
+		log.Fatal("Must set DATABASE_NAME")
+	}
+
+	router.HandleFunc("/", GetIndex).Methods("GET")
+	router.HandleFunc("/services", GetServices).Methods("GET")
+	router.HandleFunc("/services/new", PostServices).Methods("POST")
+
+	http.Handle("/", router)
+}
+
+func main() {
+	// Use all CPU cores
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	// Start HTTP server
+	server := SimpleHTTPServer(router, ":9090")
+	log.Printf("HTTP server trying to listen on %v...\n", server.Addr)
+	if err := server.ListenAndServe(); err != nil {
+		log.Printf("HTTP listen failed: %v\n", err)
+	}
+}
+
+func SimpleHTTPServer(handler http.Handler, host string) *http.Server {
+	server := http.Server{
+		Addr:           host,
+		Handler:        handler,
+		ReadTimeout:    30 * time.Second,
+		WriteTimeout:   30 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	return &server
+}
+
+func GetIndex(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to Hyperactive! Check out /services\n"))
+}
+
+func GetServices(w http.ResponseWriter, r *http.Request) {
+	// Grab all HypeService objects from DB
+	services, err := types.ServicesList()
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+	// Marshall all HypeService ~objects to JSON
+	jsonStr, err := json.Marshal(services)
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Write(jsonStr)
+}
+
+func PostServices(w http.ResponseWriter, r *http.Request) {
+	// Read POSTed body (should be JSON)
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+	defer r.Body.Close()
+
+	hs := &types.HypeService{}
+	// Unmarshal JSON into TentServer var
+	if err := json.Unmarshal(body, hs); err != nil {
+		writeError(w, err)
+		return
+	}
+	// Store new HypeService
+	if hs.URL == FAKE_URL {
+		writeError(w, fmt.Errorf("Are you _sure_ that's the right URL?\n"))
+		return
+	}
+
+	// Store to DB
+	if err = hs.Save(); err != nil {
+		writeError(w, err)
+		return
+	}
+
+	// Marshal to JSON and return to user
+	jsonData, err := json.Marshal(hs)
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write(jsonData)
+}
+
+func writeError(w http.ResponseWriter, err error) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	http.Error(w, err.Error(), 500)
+}
---
+++ types/types.go
@@ -0,0 +1,75 @@
+// Steve Phillips / elimisteve
+// 2013.11.10
+
+package types
+
+import (
+	"fmt"
+	"time"
+)
+
+var (
+	// TODO: Replace with legit DB
+	hypeServices = map[string]*HypeService{} // map from URL to *HypeService
+)
+
+type HypeService struct {
+	// POSTed by user
+	Name        string `json:"name"`
+	URL         string `json:"url"`
+	Description string `json:"description"`
+
+	// Filled in by this server
+	PostedBy   string    `json:"posted_by"`   // Read IP of original POSTer
+	ModifiedBy string    `json:"modified_by"` // Read IP of updater
+	CreatedAt  time.Time `json:"created_at"`
+	ModifiedAt time.Time `json:"modified_at"`
+	LastSeen   time.Time `json:"last_seen"`
+}
+
+func (hs *HypeService) Save() error {
+	if hs.Name == "" || hs.URL == "" || hs.Description == "" {
+		return fmt.Errorf("name, url, and description fields must be populated\n")
+	}
+
+	oldHS, found := hypeServices[hs.URL]
+	if !found {
+		if err := hs.populateFields(); err != nil {
+			return err
+		}
+		hypeServices[hs.URL] = hs
+		return nil
+	}
+	if err := hs.populateFields(); err != nil {
+		return err
+	}
+	hs.updateFromOld(oldHS)
+	hypeServices[hs.URL] = hs
+	return nil
+}
+
+func (hs *HypeService) populateFields() error {
+	now := time.Now()
+	// TODO: Only populate if blank
+	hs.PostedBy = ""   // TODO: List IP of POSTer
+	hs.ModifiedBy = "" // TODO: List IP of POSTer
+	hs.ModifiedAt = now
+	hs.CreatedAt = now
+	return nil
+}
+
+func (hs *HypeService) updateFromOld(oldHS *HypeService) {
+	// New version wins, except `PostedBy` and `CreatedAt` fields
+	hs.PostedBy = oldHS.PostedBy
+	hs.CreatedAt = oldHS.CreatedAt
+}
+
+func ServicesList() ([]*HypeService, error) {
+	list := make([]*HypeService, len(hypeServices))
+	var ndx int
+	for _, hs := range hypeServices {
+		list[ndx] = hs
+		ndx++
+	}
+	return list, nil
+}
