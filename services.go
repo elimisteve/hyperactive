cdb96e914f102ec4e7290df598e052c60a2328f7
Steve Phillips <elimisteve@gmail.com>
2013-11-10 23:41:11 -0800

    PostServices: Fixed content-type (text -> JSON)

--- services.go
+++ services.go
@@ -99,7 +99,7 @@ func PostServices(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.Write(jsonData)
 }
 
